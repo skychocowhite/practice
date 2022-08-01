7a7302eaecb89ab0e70f42ccb49b3c212c881444
skychocowhite <simon2000.wang@gmail.com>
2022-08-02 05:45:33 +0800

    Add test content in new branch

--- java/Main.java
+++ java/Main.java
@@ -1,5 +1,7 @@
 public class Main {
 	public static void main(String[] args) {
+		Test test = new Test();
+		test.call();
 		System.out.println("Hello world!");
 	}
 }
---
+++ java/Test.java
@@ -0,0 +1,7 @@
+public class Test {
+	private static String message = "This is a test message from test branch";
+
+	public void call() {
+		System.out.println(Test.message);
+	}
+}
