e92a04cb454250b7a3d488e379aaa66e2bd9e394
skychocowhite <simon2000.wang@gmail.com>
2022-08-02 05:57:12 +0800

    Other test branch

--- java/Test.java
+++ java/Test.java
@@ -1,7 +1,9 @@
 public class Test {
 	private static String message = "This is a test message from test branch";
+	private String yetMessage = "Another message from another test branch";
 
 	public void call() {
 		System.out.println(Test.message);
+		System.out.println(yetMessage);
 	}
 }
